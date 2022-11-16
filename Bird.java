b12ac9f90381bd4480938fe53c7478c809950610
Thespiann <100039436+Thespiann@users.noreply.github.com>
2022-11-16 13:07:35 +0200

    Delete Bird.java

--- Bird.java
+++
@@ -1,9 +0,0 @@
-package com.myproject;
-
-public class Bird extends Animal implements AnimalActions {
-    @Override
-    public void learnAboutClass() {//Μέθοδος εκτύπωσης πληροφοριωνΚ της κλασης Bird, ομοιως για τις υπολοιπες κλασεις παιδια
-        System.out.println("//////About Birds");
-        System.out.println("Birds are a group of warm-blooded vertebrates characterised\n by feathers, toothless beaked jaws, the laying of hard-shelled eggs,\na high metabolic rate, a four-chambered heart, and a strong yet\n lightweight skeleton.");
-    }
-}
